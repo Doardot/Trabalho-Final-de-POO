7cf458e8ab098ac1e8ca710b3d8abcbf8499ebb0
Bernardo Alves <bernardoalves0131@gmail.com>
2023-11-07 21:48:41 -0300

    adicionando os construtores

--- src/Seca.java
+++ src/Seca.java
@@ -2,4 +2,10 @@ public class Seca extends Evento {
 
 	private int estiagem;
 
+	public Seca(String codigo, String data, double latitude, double longitude, Atendimento atendimento, int estiagem) {
+		super(codigo, data, latitude, longitude, atendimento);
+		this.estiagem = estiagem;
+	}
+
+	
 }
--- src/Terremoto.java
+++ src/Terremoto.java
@@ -2,4 +2,13 @@ public class Terremoto extends Evento {
 
 	private double magnitude;
 
+	public Terremoto(String codigo, String data, double latitude, double longitude, Atendimento atendimento,
+			double magnitude) {
+		super(codigo, data, latitude, longitude, atendimento);
+		this.magnitude = magnitude;
+	}
+
+
+
+	
 }
